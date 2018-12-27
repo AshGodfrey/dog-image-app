9f248cdf78c90dadf83dfdd111e03f95722af360
Victoria Godfrey <victorialgodfrey@gmail.com>
2018-12-27 09:08:58 -0500

    created simple dog API to display to console

---
+++ index.js
@@ -0,0 +1,39 @@
+function getDogImages(){
+	let userValue = $("#number-of-dogs").val();
+	if ($.isNumeric(userValue)){
+		if (userValue > 50 || userValue < 1) {
+			alert ("you must choose a number between 1 and 50")
+		} else {
+			fetch (`https://dog.ceo/api/breeds/image/random/${userValue}`)
+			 .then(response => response.json())
+		    .then(responseJson => 
+		      displayResults(responseJson))
+		    .catch(error => alert('Something went wrong. Try again later.'));
+		}
+	} else {
+		alert ("you must enter a whole number")
+	}
+	
+}
+
+function submitForm() {
+	$('form').submit(event => {
+    event.preventDefault();
+    getDogImages();
+  });
+}
+
+function displayResults(responseJson) {
+	console.log(responseJson);
+	 $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+
+
+submitForm();
+
+
